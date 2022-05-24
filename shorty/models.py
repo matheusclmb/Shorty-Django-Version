dafda81a2f7db2243589ffbdbf5cb1130c245bbe
Matheus Colombo <ccolombomatheus@gmail.com>
2022-05-24 23:39:44 +0100

    🚀 feat: Create Class on Models

--- shorty/models.py
+++ shorty/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Shortener(models.Model):
+    url = models.URLField()
+    short_url = models.CharField(max_length=6, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    redirects = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.short_url
