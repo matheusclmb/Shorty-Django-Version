d4b29b782b98bc1232b47bfd206903e79c73e890
Matheus Colombo <ccolombomatheus@gmail.com>
2022-05-24 23:46:07 +0100

    🐛 fix: Classes names on Models

--- shorty/models.py
+++ shorty/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class Shortener(models.Model):
     url = models.URLField()
-    short_url = models.CharField(max_length=6, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    redirects = models.PositiveIntegerField(default=0)
+    shortcode = models.CharField(max_length=6, unique=True)
+    startDate = models.DateTimeField(auto_now_add=True)
+    lastSeenDate = models.DateTimeField(auto_now=True)
+    redirectCount = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.short_url
